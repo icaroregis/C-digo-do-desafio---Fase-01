ea6efeb0fb711f49f103277a393bade4a84ae10c
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-06-01 21:27:13 -0300

    ...

---
+++ prova6.js
@@ -0,0 +1,6 @@
+var z = 0;
+for (var i = 20; i < 50; i += 10) {
+  z += i;
+}
+
+console.log(z);
---
+++ prova7.js
@@ -0,0 +1,8 @@
+for (var i = 0; i < 50; i += 10) {
+  console.log(i);
+}
+
+let number = 2;
+if (number > 5) {
+  console.log('teste');
+}
