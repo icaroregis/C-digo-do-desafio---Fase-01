871fcc2626e60d6ed19c4c897b85597492f382e3
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-06-01 21:58:14 -0300

    ...

--- prova.js
+++
@@ -1,5 +0,0 @@
-let numeros = [10, 8, 7, 99, 0, 1, 6];
-
-numeros.sort((a, b) => b - a);
-
-console.log(numeros);
--- prova7.js
+++
@@ -1,8 +0,0 @@
-for (var i = 0; i < 50; i += 10) {
-  console.log(i);
-}
-
-let number = 2;
-if (number > 5) {
-  console.log('teste');
-}
